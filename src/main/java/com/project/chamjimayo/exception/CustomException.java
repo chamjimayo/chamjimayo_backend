5203cab40d0b7c7b6ecaa15e3ab6b6f74e6795db
HyunTae Jo <hyuntae9912@naver.com>
2023-07-12 11:23:13 +0900

    통합 예외 관리를 위한 ExceptionHandler 추가 (일단 간단한 오류에 대해서만 처리함)

---
+++ src/main/java/com/project/chamjimayo/exception/CustomException.java
@@ -0,0 +1,15 @@
+package com.project.chamjimayo.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private ErrorCode errorCode;
+
+    public CustomException(String message, ErrorCode errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
+
