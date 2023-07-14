e65c5aad9f2cc2ec5776a0d123bb53e0c801a559
HyunTae Jo <hyuntae9912@naver.com>
2023-07-14 23:14:29 +0900

    error log 추가

--- src/main/java/com/project/chamjimayo/controller/GlobalExceptionHandler.java
+++ src/main/java/com/project/chamjimayo/controller/GlobalExceptionHandler.java
@@ -21,18 +21,21 @@ public class GlobalExceptionHandler {
 
 	@ExceptionHandler(ApiNotFoundException.class)
 	public ResponseEntity<ErrorResponse> handleApiNotFoundException(ApiNotFoundException e) {
+		log.error( e.getMessage());
 		ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.API_NOT_FOUND, e.getMessage());
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
 	}
 
 	@ExceptionHandler(DataAccessException.class)
 	public ResponseEntity<ErrorResponse> handleDatabaseException(DataAccessException e) {
+		log.error("데이터베이스에 오류가 발생했습니다.");
 		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.DATABASE_ERROR, "데이터베이스에 오류가 발생했습니다.");
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
 	}
 
 	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
 	public ResponseEntity<ErrorResponse> HttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+		log.error("지원하지 않는 HTTP Method입니다.");
 		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.INVALID_HTTP_METHOD, "지원하지 않는 HTTP Method입니다.");
 		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(errorResponse);
 	}
@@ -40,12 +43,14 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
 	public ResponseEntity<ErrorResponse> MethodArgumentTypeMismatchException(
 		MethodArgumentTypeMismatchException e) {
+		log.error("올바르지 않은 파라미터 값입니다.");
 		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.INVALID_PARAMETER, "올바르지 않은 파라미터 값입니다.");
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
 	}
 
 	@ExceptionHandler(JsonFileNotFoundException.class)
 	public ResponseEntity<ErrorResponse> handleInvalidJsonFileNotFoundException(JsonFileNotFoundException e) {
+		log.error( e.getMessage());
 		ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.JSON_NOT_FOUND, e.getMessage());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
 	}
@@ -54,18 +59,21 @@ public class GlobalExceptionHandler {
 	@ExceptionHandler(MissingServletRequestParameterException.class)
 	public ResponseEntity<ErrorResponse> MissingServletRequestParameterException(
 		MissingServletRequestParameterException e) {
+		log.error("파라미터가 부족합니다.");
 		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.NEED_MORE_PARAMETER, "파라미터가 부족합니다.");
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
 	}
 
 	@ExceptionHandler(SearchHistoryNotFoundException.class)
 	public ResponseEntity<ErrorResponse> handleInvalidSearchHistoryNotFoundException(SearchHistoryNotFoundException e) {
+		log.error( e.getMessage());
 		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.SEARCH_NOT_FOUND, e.getMessage());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
 	}
 
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<ErrorResponse> handleException(Exception e) {
+		log.error("예상치 못한 서버 오류가 발생했습니다.");
 		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.INTERNAL_SERVER_ERROR, "예상치 못한 서버 오류가 발생했습니다.");
 		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
 	}
