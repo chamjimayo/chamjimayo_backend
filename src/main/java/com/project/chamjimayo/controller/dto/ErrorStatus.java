379133768b6ca563900fc1c49fa4c55cb6d1b3f9
HyunTae Jo <hyuntae9912@naver.com>
2023-07-17 17:04:38 +0900

    주석 추가

--- src/main/java/com/project/chamjimayo/controller/dto/ErrorStatus.java
+++ src/main/java/com/project/chamjimayo/controller/dto/ErrorStatus.java
@@ -1,9 +1,13 @@
 package com.project.chamjimayo.controller.dto;
 
-import lombok.Getter;
 
-@Getter
-public enum ErrorCode {
+public enum ErrorStatus {
+  // 400 BAD_REQUEST 잘못된 요청
+  // 401 Unauthorized 인증되지 않음
+  // 403 Forbidden 권한 없음
+  // 404 NOT_FOUND 잘못된 리소스 접근
+  // 405 METHOD_NOT_ALLOWED 지원되지 않는 HTTP 요청 메서드
+  // 500 INTERNAL SERVER ERROR 서버 오류
   INVALID_PARAMETER("01"),
   NEED_MORE_PARAMETER("02"),
   USER_DUPLICATE_EXCEPTION("03"),
@@ -24,7 +28,7 @@ public enum ErrorCode {
 
   private final String code;
 
-  ErrorCode(String code) {
+  ErrorStatus(String code) {
     this.code = code;
   }
 
