cb1905e7743c44e688dd33e1c9aff0027b260ac6
HyunTae Jo <hyuntae9912@naver.com>
2023-07-17 13:40:13 +0900

    ReviewController ApiStandardResponse 형식으로 응답

--- src/main/java/com/project/chamjimayo/controller/ReviewController.java
+++ src/main/java/com/project/chamjimayo/controller/ReviewController.java
@@ -1,5 +1,6 @@
 package com.project.chamjimayo.controller;
 
+import com.project.chamjimayo.controller.dto.ApiStandardResponse;
 import com.project.chamjimayo.controller.dto.ErrorResponse;
 import com.project.chamjimayo.controller.dto.ReviewDto;
 import com.project.chamjimayo.controller.dto.UpdateReviewDto;
@@ -12,18 +13,11 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "review", description = "리뷰 API")
 @RequiredArgsConstructor
@@ -39,13 +33,14 @@ public class ReviewController {
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReviewDto.class))),
 		@ApiResponse(responseCode = "404", description = "1. 화장실을 찾을 수 없습니다. \t\n2. 유저를 찾지 못했습니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"RESTROOM_NOT_FOUND\", \"msg\": \"화장실을 찾을 수 없습니다.\" }")))
+				examples = @ExampleObject(value = "{ \"code\": \"RESTROOM_NOT_FOUND\", \"message\": \"화장실을 찾을 수 없습니다.\" }")))
 	})
 	@PostMapping
-	public ResponseEntity<ReviewDto> createReview(
+	public ResponseEntity<ApiStandardResponse<ReviewDto>> createReview(
 		@RequestBody ReviewDto reviewDto) {
 		ReviewDto createdReview = reviewService.createReview(reviewDto);
-		return ResponseEntity.status(HttpStatus.CREATED).body(createdReview);
+		ApiStandardResponse<ReviewDto> apiStandardResponse = ApiStandardResponse.success(createdReview);
+		return ResponseEntity.ok(apiStandardResponse);
 	}
 
 	@Operation(summary = "리뷰 조회", description = "특정 리뷰를 조회합니다.")
@@ -54,17 +49,18 @@ public class ReviewController {
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReviewDto.class))),
 		@ApiResponse(responseCode = "400", description = "1. 파라미터가 부족합니다. \t\n2. 올바르지 않은 파라미터 값입니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"msg\": \"파라미터가 부족합니다.\" }"))),
+				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"message\": \"파라미터가 부족합니다.\" }"))),
 		@ApiResponse(responseCode = "404", description = "리뷰를 찾을 수 없습니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"REVIEW_NOT_FOUND\", \"msg\": \"리뷰를 찾을 수 없습니다.\" }")))
+				examples = @ExampleObject(value = "{ \"code\": \"REVIEW_NOT_FOUND\", \"message\": \"리뷰를 찾을 수 없습니다.\" }")))
 	})
 	@GetMapping("/{reviewId}")
-	public ResponseEntity<ReviewDto> getReview(
+	public ResponseEntity<ApiStandardResponse<ReviewDto>> getReview(
 		@Parameter(description = "리뷰 ID", required = true, example = "1 (Long)")
 		@PathVariable Long reviewId) {
 		ReviewDto reviewDto = reviewService.getReview(reviewId);
-		return ResponseEntity.ok(reviewDto);
+		ApiStandardResponse<ReviewDto> apiStandardResponse = ApiStandardResponse.success(reviewDto);
+		return ResponseEntity.ok(apiStandardResponse);
 	}
 
 	@Operation(summary = "리뷰 수정", description = "기존 리뷰를 수정합니다.")
@@ -73,18 +69,19 @@ public class ReviewController {
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReviewDto.class))),
 		@ApiResponse(responseCode = "400", description = "1. 파라미터가 부족합니다. \t\n2. 올바르지 않은 파라미터 값입니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"msg\": \"파라미터가 부족합니다.\" }"))),
+				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"message\": \"파라미터가 부족합니다.\" }"))),
 		@ApiResponse(responseCode = "404", description = "리뷰를 찾을 수 없습니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"REVIEW_NOT_FOUND\", \"msg\": \"리뷰를 찾을 수 없습니다.\" }")))
+				examples = @ExampleObject(value = "{ \"code\": \"REVIEW_NOT_FOUND\", \"message\": \"리뷰를 찾을 수 없습니다.\" }")))
 	})
 	@PatchMapping("/{reviewId}")
-	public ResponseEntity<ReviewDto> updateReview(
+	public ResponseEntity<ApiStandardResponse<ReviewDto>> updateReview(
 		@Parameter(description = "리뷰 ID", required = true, example = "1 (Long)")
 		@PathVariable Long reviewId,
 		@RequestBody UpdateReviewDto updateReviewDto) {
 		ReviewDto updatedReview = reviewService.updateReview(reviewId, updateReviewDto);
-		return ResponseEntity.ok(updatedReview);
+		ApiStandardResponse<ReviewDto> apiStandardResponse = ApiStandardResponse.success(updatedReview);
+		return ResponseEntity.ok(apiStandardResponse);
 	}
 
 	@Operation(summary = "리뷰 삭제", description = "특정 리뷰를 삭제합니다.")
@@ -92,17 +89,18 @@ public class ReviewController {
 		@ApiResponse(responseCode = "200", description = "리뷰 삭제 성공"),
 		@ApiResponse(responseCode = "400", description = "1. 파라미터가 부족합니다. \t\n2. 올바르지 않은 파라미터 값입니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"msg\": \"파라미터가 부족합니다.\" }"))),
+				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"message\": \"파라미터가 부족합니다.\" }"))),
 		@ApiResponse(responseCode = "404", description = "리뷰를 찾을 수 없습니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"REVIEW_NOT_FOUND\", \"msg\": \"리뷰를 찾을 수 없습니다.\" }")))
+				examples = @ExampleObject(value = "{ \"code\": \"REVIEW_NOT_FOUND\", \"message\": \"리뷰를 찾을 수 없습니다.\" }")))
 	})
 	@DeleteMapping("/{reviewId}")
-	public ResponseEntity<String> deleteReview(
+	public ResponseEntity<ApiStandardResponse<String>> deleteReview(
 		@Parameter(description = "리뷰 ID", required = true, example = "1 (Long)")
 		@PathVariable Long reviewId) {
 		reviewService.deleteReview(reviewId);
-		return ResponseEntity.ok("리뷰 삭제 성공");
+		ApiStandardResponse<String> apiStandardResponse = ApiStandardResponse.success("리뷰 삭제 성공");
+		return ResponseEntity.ok(apiStandardResponse);
 	}
 
 	@Operation(summary = "해당 화장실의 모든 리뷰 조회 (기본)", description = "특정 화장실에 해당하는 모든 리뷰를 조회합니다.")
@@ -111,17 +109,18 @@ public class ReviewController {
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReviewDto.class))),
 		@ApiResponse(responseCode = "400", description = "1. 파라미터가 부족합니다. \t\n2. 올바르지 않은 파라미터 값입니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"msg\": \"파라미터가 부족합니다.\" }"))),
+				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"message\": \"파라미터가 부족합니다.\" }"))),
 		@ApiResponse(responseCode = "404", description = "화장실을 찾을 수 없습니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"RESTROOM_NOT_FOUND\", \"msg\": \"화장실을 찾을 수 없습니다.\" }")))
+				examples = @ExampleObject(value = "{ \"code\": \"RESTROOM_NOT_FOUND\", \"message\": \"화장실을 찾을 수 없습니다.\" }")))
 	})
 	@GetMapping("/list/{restroomId}")
-	public ResponseEntity<List<ReviewDto>> getReviewsByRestroomId(
+	public ResponseEntity<ApiStandardResponse<List<ReviewDto>>> getReviewsByRestroomId(
 		@Parameter(description = "화장실 ID", required = true, example = "1 (Long)")
 		@PathVariable Long restroomId) {
 		List<ReviewDto> reviewDtoList = reviewService.getReviewsByRestroomId(restroomId);
-		return ResponseEntity.ok(reviewDtoList);
+		ApiStandardResponse<List<ReviewDto>> apiStandardResponse = ApiStandardResponse.success(reviewDtoList);
+		return ResponseEntity.ok(apiStandardResponse);
 	}
 
 	@Operation(summary = "해당 화장실의 모든 리뷰 조회 (별점 높은 순)", description = "특정 화장실에 해당하는 모든 리뷰를 별점이 높은 순으로 조회합니다.")
@@ -130,18 +129,18 @@ public class ReviewController {
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReviewDto.class))),
 		@ApiResponse(responseCode = "400", description = "1. 파라미터가 부족합니다. \t\n2. 올바르지 않은 파라미터 값입니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"msg\": \"파라미터가 부족합니다.\" }"))),
+				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"message\": \"파라미터가 부족합니다.\" }"))),
 		@ApiResponse(responseCode = "404", description = "화장실을 찾을 수 없습니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"RESTROOM_NOT_FOUND\", \"msg\": \"화장실을 찾을 수 없습니다.\" }")))
+				examples = @ExampleObject(value = "{ \"code\": \"RESTROOM_NOT_FOUND\", \"message\": \"화장실을 찾을 수 없습니다.\" }")))
 	})
 	@GetMapping("/list/{restroomId}/high-rating")
-	public ResponseEntity<List<ReviewDto>> getReviewsByRestroomIdOrderByHighRating(
+	public ResponseEntity<ApiStandardResponse<List<ReviewDto>>> getReviewsByRestroomIdOrderByHighRating(
 		@Parameter(description = "화장실 ID", required = true, example = "1 (Long)")
 		@PathVariable Long restroomId) {
-		List<ReviewDto> reviewDtoList = reviewService.getReviewsByRestroomIdOrderByHighRating(
-			restroomId);
-		return ResponseEntity.ok(reviewDtoList);
+		List<ReviewDto> reviewDtoList = reviewService.getReviewsByRestroomIdOrderByHighRating(restroomId);
+		ApiStandardResponse<List<ReviewDto>> apiStandardResponse = ApiStandardResponse.success(reviewDtoList);
+		return ResponseEntity.ok(apiStandardResponse);
 	}
 
 	@Operation(summary = "해당 화장실의 모든 리뷰 조회 (별점 낮은 순)", description = "특정 화장실에 해당하는 모든 리뷰를 별점이 낮은 순으로 조회합니다.")
@@ -150,18 +149,18 @@ public class ReviewController {
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReviewDto.class))),
 		@ApiResponse(responseCode = "400", description = "1. 파라미터가 부족합니다. \t\n2. 올바르지 않은 파라미터 값입니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"msg\": \"파라미터가 부족합니다.\" }"))),
+				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"message\": \"파라미터가 부족합니다.\" }"))),
 		@ApiResponse(responseCode = "404", description = "화장실을 찾을 수 없습니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"RESTROOM_NOT_FOUND\", \"msg\": \"화장실을 찾을 수 없습니다.\" }")))
+				examples = @ExampleObject(value = "{ \"code\": \"RESTROOM_NOT_FOUND\", \"message\": \"화장실을 찾을 수 없습니다.\" }")))
 	})
 	@GetMapping("/list/{restroomId}/low-rating")
-	public ResponseEntity<List<ReviewDto>> getReviewsByRestroomIdOrderByLowRating(
+	public ResponseEntity<ApiStandardResponse<List<ReviewDto>>> getReviewsByRestroomIdOrderByLowRating(
 		@Parameter(description = "화장실 ID", required = true, example = "1 (Long)")
 		@PathVariable Long restroomId) {
-		List<ReviewDto> reviewDtoList = reviewService.getReviewsByRestroomIdOrderByLowRating(
-			restroomId);
-		return ResponseEntity.ok(reviewDtoList);
+		List<ReviewDto> reviewDtoList = reviewService.getReviewsByRestroomIdOrderByLowRating(restroomId);
+		ApiStandardResponse<List<ReviewDto>> apiStandardResponse = ApiStandardResponse.success(reviewDtoList);
+		return ResponseEntity.ok(apiStandardResponse);
 	}
 
 	@Operation(summary = "해당 화장실의 평균 평점", description = "해당 화장실의 평균 평점을 조회합니다.")
@@ -170,16 +169,17 @@ public class ReviewController {
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ReviewDto.class))),
 		@ApiResponse(responseCode = "400", description = "1. 파라미터가 부족합니다. \t\n2. 올바르지 않은 파라미터 값입니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"msg\": \"파라미터가 부족합니다.\" }"))),
+				examples = @ExampleObject(value = "{ \"code\": \"NEED_MORE_PARAMETER\", \"message\": \"파라미터가 부족합니다.\" }"))),
 		@ApiResponse(responseCode = "404", description = "화장실을 찾을 수 없습니다.",
 			content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorResponse.class),
-				examples = @ExampleObject(value = "{ \"code\": \"RESTROOM_NOT_FOUND\", \"msg\": \"화장실을 찾을 수 없습니다.\" }")))
+				examples = @ExampleObject(value = "{ \"code\": \"RESTROOM_NOT_FOUND\", \"message\": \"화장실을 찾을 수 없습니다.\" }")))
 	})
 	@GetMapping("/rating/{restroomId}")
-	public Float averageRating(
+	public ResponseEntity<ApiStandardResponse<Float>> averageRating(
 		@Parameter(description = "화장실 ID", required = true, example = "1 (Long)")
 		@PathVariable Long restroomId) {
 		Float averageRating = reviewService.averageRating(restroomId);
-		return averageRating;
+		ApiStandardResponse<Float> apiStandardResponse = ApiStandardResponse.success(averageRating);
+		return ResponseEntity.ok(apiStandardResponse);
 	}
 }
