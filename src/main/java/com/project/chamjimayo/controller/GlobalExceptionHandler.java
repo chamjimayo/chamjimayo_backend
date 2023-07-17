86b855fc3cc8358811aad214a62c0eb0574fa9cc
HyunTae Jo <hyuntae9912@naver.com>
2023-07-17 13:26:19 +0900

    1. ErrorCode 상세화 2. Response 통일

--- src/main/java/com/project/chamjimayo/controller/GlobalExceptionHandler.java
+++ src/main/java/com/project/chamjimayo/controller/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.project.chamjimayo.controller;
 
+import com.project.chamjimayo.controller.dto.ApiStandardResponse;
 import com.project.chamjimayo.controller.dto.ErrorCode;
 import com.project.chamjimayo.controller.dto.ErrorResponse;
 import com.project.chamjimayo.exception.ApiNotFoundException;
@@ -7,6 +8,7 @@ import com.project.chamjimayo.exception.JsonFileNotFoundException;
 import com.project.chamjimayo.exception.RestroomNotFoundException;
 import com.project.chamjimayo.exception.ReviewNotFoundException;
 import com.project.chamjimayo.exception.SearchHistoryNotFoundException;
+import javax.validation.ValidationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
@@ -21,76 +23,99 @@ import org.springframework.web.method.annotation.MethodArgumentTypeMismatchExcep
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+	@ExceptionHandler(SearchHistoryNotFoundException.class)
+	public ResponseEntity<ApiStandardResponse<ErrorResponse>> handleInvalidSearchHistoryNotFoundException(
+		SearchHistoryNotFoundException e) {
+		log.error(e.getMessage());
+		ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.SEARCH_NOT_FOUND, e.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+			.body(ApiStandardResponse.fail(errorResponse));
+	}
+
 	@ExceptionHandler(ApiNotFoundException.class)
-	public ResponseEntity<ErrorResponse> handleApiNotFoundException(ApiNotFoundException e) {
-		log.error( e.getMessage());
+	public ResponseEntity<ApiStandardResponse<ErrorResponse>> handleApiNotFoundException(ApiNotFoundException e) {
+		log.error(e.getMessage());
 		ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.API_NOT_FOUND, e.getMessage());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+			.body(ApiStandardResponse.fail(errorResponse));
 	}
 
 	@ExceptionHandler(JsonFileNotFoundException.class)
-	public ResponseEntity<ErrorResponse> handleInvalidJsonFileNotFoundException(JsonFileNotFoundException e) {
-		log.error( e.getMessage());
+	public ResponseEntity<ApiStandardResponse<ErrorResponse>> handleInvalidJsonFileNotFoundException(
+		JsonFileNotFoundException e) {
+		log.error(e.getMessage());
 		ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.JSON_NOT_FOUND, e.getMessage());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
-	}
-
-	@ExceptionHandler(SearchHistoryNotFoundException.class)
-	public ResponseEntity<ErrorResponse> handleInvalidSearchHistoryNotFoundException(SearchHistoryNotFoundException e) {
-		log.error( e.getMessage());
-		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.SEARCH_NOT_FOUND, e.getMessage());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+			.body(ApiStandardResponse.fail(errorResponse));
 	}
 
 	@ExceptionHandler(ReviewNotFoundException.class)
-	public ResponseEntity<ErrorResponse> handleReviewNotFoundException(ReviewNotFoundException e) {
-		log.error( e.getMessage());
-		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.REVIEW_NOT_FOUND, e.getMessage());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+	public ResponseEntity<ApiStandardResponse<ErrorResponse>> handleReviewNotFoundException(
+		ReviewNotFoundException e) {
+		log.error(e.getMessage());
+		ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.REVIEW_NOT_FOUND, e.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+			.body(ApiStandardResponse.fail(errorResponse));
 	}
 
 	@ExceptionHandler(RestroomNotFoundException.class)
-	public ResponseEntity<ErrorResponse> handleRestroomNotFoundException(RestroomNotFoundException e) {
-		log.error( e.getMessage());
-		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.RESTROOM_NOT_FOUND, e.getMessage());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+	public ResponseEntity<ApiStandardResponse<ErrorResponse>> handleRestroomNotFoundException(
+		RestroomNotFoundException e) {
+		log.error(e.getMessage());
+		ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.RESTROOM_NOT_FOUND, e.getMessage());
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+			.body(ApiStandardResponse.fail(errorResponse));
 	}
 
 	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-	public ResponseEntity<ErrorResponse> HttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+	public ResponseEntity<ApiStandardResponse<ErrorResponse>> HttpRequestMethodNotSupportedException(
+		HttpRequestMethodNotSupportedException e) {
 		log.error("지원하지 않는 HTTP Method입니다.");
-		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.METHOD_NOT_ALLOWED_EXCEPTION, "지원하지 않는 HTTP Method입니다.");
-		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(errorResponse);
+		ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.METHOD_NOT_ALLOWED_EXCEPTION, "지원하지 않는 HTTP Method입니다.");
+		return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED)
+			.body(ApiStandardResponse.fail(errorResponse));
+	}
+
+	@ExceptionHandler(ValidationException.class)
+	public ResponseEntity<ApiStandardResponse<ErrorResponse>> handleValidationException(ValidationException e) {
+		log.error("유효성 검사에 실패했습니다.");
+		ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.INVALID_PARAMETER, "올바른 파라미터를 입력하세요.");
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(ApiStandardResponse.fail(errorResponse));
 	}
 
 	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
-	public ResponseEntity<ErrorResponse> MethodArgumentTypeMismatchException(
+	public ResponseEntity<ApiStandardResponse<ErrorResponse>> MethodArgumentTypeMismatchException(
 		MethodArgumentTypeMismatchException e) {
 		log.error("올바르지 않은 파라미터 값입니다.");
-		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.INVALID_PARAMETER, "올바르지 않은 파라미터 값입니다.");
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+		ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.INVALID_PARAMETER, "올바르지 않은 파라미터 값입니다.");
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(ApiStandardResponse.fail(errorResponse));
 	}
 
 	@ExceptionHandler(MissingServletRequestParameterException.class)
-	public ResponseEntity<ErrorResponse> MissingServletRequestParameterException(
+	public ResponseEntity<ApiStandardResponse<ErrorResponse>> MissingServletRequestParameterException(
 		MissingServletRequestParameterException e) {
 		log.error("파라미터가 부족합니다.");
-		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.NEED_MORE_PARAMETER, "파라미터가 부족합니다.");
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+		ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.NEED_MORE_PARAMETER, "파라미터가 부족합니다.");
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+			.body(ApiStandardResponse.fail(errorResponse));
 	}
 
 	@ExceptionHandler(DataAccessException.class)
-	public ResponseEntity<ErrorResponse> handleDatabaseException(DataAccessException e) {
+	public ResponseEntity<ApiStandardResponse<ErrorResponse>> handleDatabaseException(DataAccessException e) {
 		log.error("데이터베이스에 오류가 발생했습니다.");
-		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.DATABASE_ERROR, "데이터베이스에 오류가 발생했습니다.");
-		return ResponseEntity.status(501).body(errorResponse);
+		ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.DATABASE_ERROR, "데이터베이스에 오류가 발생했습니다.");
+		return ResponseEntity.status(501)
+			.body(ApiStandardResponse.fail(errorResponse));
 	}
 
-//	// 너무 많은 오류를 처리해서 test 단계에서는 일단 주석 처리하겠습니다.
+	// test 단계에서 예상치 못한 오류 파악을 위한 주석 처리
 //	@ExceptionHandler(Exception.class)
-//	public ResponseEntity<ErrorResponse> handleException(Exception e) {
+//	public ResponseEntity<ApiStandardResponse<ErrorResponse>> handleException(Exception e) {
 //		log.error("서버 내부 오류가 발생했습니다.");
-//		final ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
-//		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(errorResponse);
+//		ErrorResponse errorResponse = ErrorResponse.create(ErrorCode.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생했습니다.");
+//		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+//			.body(ApiStandardResponse.fail(errorResponse));
 //	}
 }
