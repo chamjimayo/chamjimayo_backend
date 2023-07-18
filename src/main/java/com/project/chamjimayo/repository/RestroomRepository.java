0385d5e9922593e5af29bf1fd0ca3e4d4d0d9401
HyunTae Jo <hyuntae9912@naver.com>
2023-07-18 14:51:43 +0900

    주석 정리

--- src/main/java/com/project/chamjimayo/repository/RestroomRepository.java
+++ src/main/java/com/project/chamjimayo/repository/RestroomRepository.java
@@ -2,7 +2,6 @@ package com.project.chamjimayo.repository;
 
 
 import com.project.chamjimayo.domain.entity.Restroom;
-import com.project.chamjimayo.domain.entity.User;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
