14f79e6ff7fff9fcab7b947018735e42498f8621
HyunTae Jo <hyuntae9912@naver.com>
2023-07-19 21:56:25 +0900

    SecurityConfig 엔드포인트 수정

--- src/main/java/com/project/chamjimayo/security/config/SecurityConfig.java
+++ src/main/java/com/project/chamjimayo/security/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig {
     http.exceptionHandling()
         .authenticationEntryPoint(new RestAuthenticationEntryPoint());
 
-    http.requestMatchers(request -> request.antMatchers("/api/users/me/**", "/api/address/search/**", "/api/review/*", "/api/users/point/**")
+    http.requestMatchers(request -> request.antMatchers("/api/users/me/**", "/api/address/search/**", "/api/review", "/api/review/*", "/api/users/point/**")
             .and()
             .addFilter(apiKeyAuthenticationFilter())
             .addFilterBefore(authenticationExceptionFilter(), ApiKeyAuthenticationFilter.class)
