8261ba000339ee9dba7d919101981bf9677de97f
HyunTae Jo <hyuntae9912@naver.com>
2023-07-23 16:13:49 +0900

    swagger 필드 설명 수정

--- src/main/java/com/project/chamjimayo/controller/dto/SearchResponseDto.java
+++ src/main/java/com/project/chamjimayo/controller/dto/SearchResponseDto.java
@@ -6,17 +6,17 @@ import lombok.Getter;
 @Getter
 public class SearchResponseDto {
 
-	@Schema(type = "string", example = "검색 키워드")
+	@Schema(type = "string", example = "스타벅스")
 	private final String searchWord;
-	@Schema(type = "string", example = "도로명 주소")
+	@Schema(type = "string", example = "서울특별시 용산구 동자로 123")
 	private final String roadAddress;
-	@Schema(type = "string", example = "지번 주소")
+	@Schema(type = "string", example = "서울특별시 용산구 동자동 12 345")
 	private final String lotNumberAddress;
-	@Schema(type = "string", example = "해당 위치의 상호명")
+	@Schema(type = "string", example = "스타벅스 서울역점")
 	private final String name;
-	@Schema(type = "double", example = "해당 위치의 위도")
+	@Schema(type = "double", example = "12.3456")
 	private final Double latitude;
-	@Schema(type = "double", example = "해당 위치의 경도")
+	@Schema(type = "double", example = "12.3456")
 	private final Double longitude;
 
 	private SearchResponseDto(String searchWord, String roadAddress, String lotNumberAddress,
