7aeaaa4c15ca67f10c2d6f05776a8168c30ee31f
HyunTae <101180610+hyuntae99@users.noreply.github.com>
2023-08-06 12:16:56 +0900

    Fix: 리뷰 작성 (#9) (#42)
    * 화장실 세부정보 api에 화장실 평균 평점 추가
    
    * 리뷰 반환값에 수정일자 추가 반환

--- src/main/java/com/project/chamjimayo/controller/dto/ReviewResponseDto.java
+++ src/main/java/com/project/chamjimayo/controller/dto/ReviewResponseDto.java
@@ -1,7 +1,9 @@
 package com.project.chamjimayo.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.project.chamjimayo.domain.entity.Review;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
 import lombok.Getter;
 
 @Getter
@@ -22,22 +24,28 @@ public class ReviewResponseDto {
   @Schema(type = "Integer", example = "4")
   private Integer rating;
 
+  @Schema(type = "string", example = "2023-07-29")
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  private LocalDateTime dateTime;
+
   private ReviewResponseDto(Long reviewId, Long userId, Long restroomId, String reviewContent,
-      Integer rating) {
+      Integer rating, LocalDateTime dateTime) {
     this.reviewId = reviewId;
     this.userId = userId;
     this.restroomId = restroomId;
     this.reviewContent = reviewContent;
     this.rating = rating;
+    this.dateTime = dateTime;
   }
 
   public static ReviewResponseDto create(Long reviewId, Long userId, Long restroomId,
-      String reviewContent, Integer rating) {
-    return new ReviewResponseDto(reviewId, userId, restroomId, reviewContent, rating);
+      String reviewContent, Integer rating, LocalDateTime dateTime) {
+    return new ReviewResponseDto(reviewId, userId, restroomId, reviewContent, rating, dateTime);
   }
 
   public static ReviewResponseDto fromEntity(Review review) {
     return new ReviewResponseDto(review.getReviewId(), review.getUser().getUserId(),
-        review.getRestroom().getRestroomId(), review.getReviewContent(), review.getRating());
+        review.getRestroom().getRestroomId(), review.getReviewContent(), review.getRating(),
+        review.getUpdatedDate());
   }
 }
