a898b38598799947df1b44122c8d6132cc9edb85
seokho-1116 <seokho7799@gmail.com>
2023-08-22 21:43:28 +0900

    Fix #8 "사용자 페이지" 1. 현재 이용중인 화장실, 화장실 이용내역 응답에 포인트와 화장실 사진 추가

--- src/main/java/com/project/chamjimayo/repository/RestroomQueryRepository.java
+++ src/main/java/com/project/chamjimayo/repository/RestroomQueryRepository.java
@@ -3,11 +3,13 @@ package com.project.chamjimayo.repository;
 import static com.project.chamjimayo.repository.domain.entity.QRestroom.restroom;
 import static com.project.chamjimayo.repository.domain.entity.QUsedRestroom.usedRestroom;
 import static com.project.chamjimayo.repository.domain.entity.QUser.user;
+import static com.project.chamjimayo.repository.domain.entity.QRestroomPhoto.restroomPhoto;
 
+import com.project.chamjimayo.repository.domain.entity.Restroom;
 import com.project.chamjimayo.service.dto.RestroomSummaryDto;
-import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import java.util.List;
+import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
@@ -24,29 +26,28 @@ public class RestroomQueryRepository {
   }
 
   public RestroomSummaryDto findUsingRestRoomDtoByUserId(Long id) {
-    RestroomSummaryDto dto = jpaQueryFactory.select(
-            Projections.constructor(RestroomSummaryDto.class,
-                restroom.restroomId, restroom.restroomName, restroom.address,
-                restroom.reviews.size(), restroom.operatingHour))
+    Restroom entity = jpaQueryFactory
+        .select(restroom)
         .from(restroom)
         .innerJoin(user).on(restroom.restroomId.eq(user.usingRestroomId))
+        .leftJoin(restroom.restroomPhotos, restroomPhoto).fetchJoin()
         .where(user.userId.eq(id).and(user.usingRestroomId.isNotNull()))
         .fetchOne();
 
-    if (dto == null) {
+    if (entity == null) {
       return RestroomSummaryDto.empty();
     }
-    return dto;
+
+    return getRestroomSummaryDto(entity);
   }
 
   public Page<RestroomSummaryDto> findUsedRestroomDtosByUserIdAndPageable
       (Long id, Pageable pageable) {
-    List<RestroomSummaryDto> dtos = jpaQueryFactory.select(
-            Projections.constructor(RestroomSummaryDto.class,
-                restroom.restroomId, restroom.restroomName, restroom.address,
-                restroom.reviews.size(), restroom.operatingHour))
+    List<Restroom> entities = jpaQueryFactory
+        .select(restroom)
         .from(restroom)
         .innerJoin(usedRestroom).on(restroom.restroomId.eq(usedRestroom.restroomId))
+        .leftJoin(restroom.restroomPhotos, restroomPhoto).fetchJoin()
         .where(usedRestroom.user.userId.eq(id))
         .offset(pageable.getOffset())
         .limit(pageable.getPageSize())
@@ -57,6 +58,15 @@ public class RestroomQueryRepository {
         .where(usedRestroom.user.userId.eq(id))
         .fetchOne();
 
+    List<RestroomSummaryDto> dtos = entities.stream()
+        .map(this::getRestroomSummaryDto)
+        .collect(Collectors.toList());
+
     return new PageImpl<>(dtos, pageable, count);
+  }
+
+  private RestroomSummaryDto getRestroomSummaryDto(Restroom r) {
+    return RestroomSummaryDto.create(r.getRestroomId(), r.getRestroomName(), r.getAddress(),
+        r.getReviews().size(), r.getOperatingHour(), r.getPrice(), r.getRestroomPhotos());
   }
 }
